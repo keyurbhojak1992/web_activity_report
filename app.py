31c92985f6ff6afea5d2cd9bc2c9bbc8abbb8418
KB <bhojakkeyur1@gmail.com>
2026-07-14 12:06:40 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Test App",
-    layout="centered",
-)
-
-st.title("Streamlit is working")
-st.success("Deployment started successfully.")
